ed100c1024fcbb79b7812ddcf8719c8856a00a69
Valinor13 <jwcalhoun2@gmail.com>
2022-04-26 17:33:33 -0500

    Updated task 3 to use recursion

--- 0x01-dart-function/3-factors.dart
+++ 0x01-dart-function/3-factors.dart
@@ -1,17 +1,29 @@
 import 'dart:math';
 
-// factor computes the factors of a positive integer
+// recursiveHelper - returns the number of factors of f
+// @factor: the number to factor
+// @counter: the number to divide by
+// @loops: the number of times to recurse
+// @factors: the running count of factors
+// @return: int factors
+int recursiveHelper(int factor, int counter, int loops, int factors) {
+  if (loops == 0) {
+    return factors;
+  } else {
+    if (factor % counter == 0) {
+      if (factor / counter != counter) {
+        factors++;
+      }
+      return recursiveHelper(factor, counter + 1, loops - 1, factors + 1);
+    } else {
+      return recursiveHelper(factor, counter + 1, loops - 1, factors);
+    }
+  }
+}
+
+// factor - using recursion, it computes the factors of a positive integer
 // @f: the positive integer to factor
 // @return: the int factors of @f
 int factor(int f) {
-  int factors = 0;
-  for (int i = 1; i <= sqrt(f); i++) {
-    if (f % i == 0) {
-      if (f / i != i) {
-        factors++;
-      }
-      factors++;
-    }
-  }
-  return factors;
+  return recursiveHelper(f, 1, sqrt(f).floor(), 0);
 }
--- 0x01-dart-function/3-main.dart
+++ 0x01-dart-function/3-main.dart
@@ -4,4 +4,6 @@ void main() {
   print(factor(0));
   print(factor(1));
   print(factor(5));
+  print(factor(10));
+  print(factor(100));
 }
