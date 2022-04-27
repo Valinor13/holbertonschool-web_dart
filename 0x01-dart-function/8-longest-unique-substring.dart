e2b5c302bf00d2964ec984b7ffa03da0747b9f1c
Valinor13 <jwcalhoun2@gmail.com>
2022-04-26 20:41:46 -0500

    Update 8-longest-unique-substring.dart

--- 0x01-dart-function/8-longest-unique-substring.dart
+++ 0x01-dart-function/8-longest-unique-substring.dart
@@ -11,46 +11,25 @@ String longestUniqueSubstring(String str) {
   Map pos = {};
   pos[str[0]] = 0;
   for (i = 1; i < strLength; i++) {
-    // If this character is not present in hash,
-    // then this is first occurrence of this
-    // character, store this in hash.
     if (pos[str[i]] == null) {
       pos[str[i]] = i;
     } else {
-      // If this character is present in hash then
-      // this character has previous occurrence,
-      // check if that occurrence is before or after
-      // starting point of current substring.
       if (pos[str[i]] >= currentStart) {
-        // find length of current substring and
-        // update maxlen and start accordingly.
         currentLength = i - currentStart;
         if (longestLength < currentLength) {
           longestLength = currentLength;
           longestStart = currentStart;
         }
-
-        // Next substring will start after the last
-        // occurrence of current character to avoid
-        // its repetition.
         currentStart = pos[str[i]] + 1;
       }
-
-      // Update last occurrence of
-      // current character.
       pos[str[i]] = i;
     }
   }
-  // Compare length of last substring with maxlen and
-  // update maxlen and start accordingly.
+
   if (longestLength < i - currentStart) {
     longestLength = i - currentStart;
     longestStart = currentStart;
   }
-
-  // The required longest substring without
-  // repeating characters is from str[start]
-  // to str[start+maxlen-1].
 
   return str.substring(longestStart, longestLength);
 }
