9922f244a3790d3e74d7d54bff5bca7ad9bec52e
Valinor13 <jwcalhoun2@gmail.com>
2022-04-28 16:16:41 -0500

    Adding task 3 chaining async functions

---
+++ 0x03-asynchronous/3-greet_user.dart
@@ -0,0 +1,31 @@
+import 'dart:convert';
+import '3-main.dart';
+
+// greetUser - calls fetchUserData() and converts to json object
+//             uses try catch to handle asnyc errors
+// returns: string hello $username
+Future<String> greetUser() async {
+  try {
+    final Map<String, dynamic> userData =
+        await json.decode(await fetchUserData());
+    return 'Hello ${userData['username']}';
+  } catch (e) {
+    return 'error caught: $e';
+  }
+}
+
+// loginUser - calls checkCredentials() and calls greetUser() if true
+//             uses try catch to handle asnyc errors
+// returns: string "Wrong credentials" if false, else returns the result
+//          of greetUser()
+Future<String> loginUser() async {
+  try {
+    if (await checkCredentials()) {
+      return await greetUser();
+    } else {
+      return 'Wrong credentials';
+    }
+  } catch (e) {
+    return 'error caught: $e';
+  }
+}
