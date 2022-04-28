db024ea7997f382f4728f91825544d660eeb705a
Valinor13 <jwcalhoun2@gmail.com>
2022-04-27 23:04:34 -0500

    Adding to task 6

---
+++ 0x02-dart-classes/6-password.dart
@@ -0,0 +1,18 @@
+class Password {
+  var _password;
+
+  String get password => _password;
+
+  set password(String password) => _password = password;
+
+  Password({required String password}) : _password = password;
+
+  bool isValid() =>
+      password.length >= 8 &&
+      password.length <= 16 &&
+      password.contains(new RegExp(r'[a-z]')) &&
+      password.contains(new RegExp(r'[A-Z]')) &&
+      password.contains(new RegExp(r'[0-9]'));
+
+  String toString() => 'Your Password is: ${password}';
+}
