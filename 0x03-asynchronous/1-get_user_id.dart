e6803b8e2165c20c1990f421a957444cb9aec2aa
Valinor13 <jwcalhoun2@gmail.com>
2022-04-28 15:49:51 -0500

    adding documentation and formatting line length

--- 0x03-asynchronous/1-get_user_id.dart
+++ 0x03-asynchronous/1-get_user_id.dart
@@ -1,8 +1,11 @@
 import 'dart:convert';
-
 import '1-main.dart';
 
+// getUserId - returns a Future<String> of the user id
+// return: returns a Future<String> of the user id
 Future<String> getUserId() async {
-  final Map<dynamic, dynamic> userData = await json.decode(await fetchUserData());
+  // get the user data and decode it from a json string to a Map object
+  final Map<dynamic, dynamic> userData =
+      await json.decode(await fetchUserData());
   return userData['id'];
 }
